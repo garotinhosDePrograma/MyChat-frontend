ef61c74c360f33e2b760c410007eb869fdc6e38e
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2026-01-21 16:04:28 -0300

    Bump version number to 1.0.31

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     API_URL: 'https://mychat-backend-m7el.onrender.com',
     
     // Versão da aplicação
-    VERSION: '1.0.30',
+    VERSION: '1.0.31',
     
     // Nome da aplicação
     APP_NAME: 'MyChat',
