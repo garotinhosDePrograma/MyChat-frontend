ac3a85eb8b6fa036249538311fdfeddbae0230c6
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2026-01-21 11:18:28 -0300

    Update application version to 1.0.30

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     API_URL: 'https://mychat-backend-m7el.onrender.com',
     
     // Versão da aplicação
-    VERSION: '1.0.27',
+    VERSION: '1.0.30',
     
     // Nome da aplicação
     APP_NAME: 'MyChat',
