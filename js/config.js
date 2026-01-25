ffdad5d97e97afb15eb77b235b5dcedea2be79ce
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2026-01-25 15:27:10 -0300

    Inacreditável

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     API_URL: 'https://mychat-backend-m7el.onrender.com',
     
     // Versão da aplicação
-    VERSION: '1.0.33',
+    VERSION: '1.0.34',
     
     // Nome da aplicação
     APP_NAME: 'MyChat',
